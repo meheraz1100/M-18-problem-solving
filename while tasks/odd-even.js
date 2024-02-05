c95fda2290667e452f1deb26897bad9784f58642
meheraz1100 <meheraz1100@gmail.com>
2024-02-05 17:33:53 +0600

    commitment problem solved

---
+++ while tasks/odd-even.js
@@ -0,0 +1,23 @@
+/*
+Subtask-1:
+Find all the odd numbers from 61 to 100.
+using js while loop
+*/
+
+
+/*
+Subtask-2:
+Find all the even numbers from 78 to 98.
+*/
+
+
+// subtask-1
+
+let i = 61;
+
+while(i <= 100){
+    if(i % 2 != 0){
+        console.log(i);
+    }
+    i++;
+}
